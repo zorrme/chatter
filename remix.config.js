8edc30f07b347c644a00519462fc896b1f25a79f
Tariq Firoz <tariq.firoz@gmail.com>
2023-02-12 08:37:51 +1100

    upgrade to route v2

--- remix.config.js
+++ remix.config.js
@@ -1,14 +1,14 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  future: {
-    v2_routeConvention: false,
-  },
   serverBuildTarget: "vercel",
   // When running locally in development mode, we use the built in remix
   // server. This does not understand the vercel lambda module format,
   // so we default back to the standard build output.
   server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   ignoredRouteFiles: ["**/.*"],
+  future: {
+    v2_routeConvention: true,
+  },
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "api/index.js",
